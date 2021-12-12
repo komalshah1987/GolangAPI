8245ce93fd954992b1224eac529213395add97fd
komalshah1987 <42387868+komalshah1987@users.noreply.github.com>
2021-12-13 00:07:27 +0530

    changes

---
+++ route/route.go
@@ -0,0 +1,34 @@
+package route
+
+import (
+	http "net/http"
+
+	"github.com/gorilla/mux"
+	v1 "github.com/komalshah1987/GolangAPI/controller"
+)
+
+type Route struct {
+	AuthMiddleware int
+	Name           string
+	Method         string
+	Pattern        string
+	HandlerFunc    http.HandlerFunc
+}
+type Routes []Route
+
+var internalRoute = Routes{
+	// 0 --> default 1--admin 2--> user
+	Route{0, "To Get list fom our syste", "GET", "/movies", v1.GetAllArticle},
+}
+
+func NewRoute() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range internalRoute {
+		switch route.AuthMiddleware {
+		case 1: // redirect this request to my middleware for checking the requst is admin or not
+		default:
+			router.Methods(route.Method).Path(route.Pattern).Name(route.Name).Handler(route.HandlerFunc)
+		}
+	}
+	return router
+}
